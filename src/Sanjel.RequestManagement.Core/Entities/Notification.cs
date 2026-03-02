813d45a2c2dca0d0643540763fd4b9d3d694c211
taoqf <tao_qiufeng@126.com>
2026-03-02 02:20:56 +0000

    refactor(Notification): Clean up formatting and remove unused namespaces
    https://github.com/Sanjel-Energy-Services/eServiceCloud/issues/777

--- src/Sanjel.RequestManagement.Core/Entities/Notification.cs
+++ src/Sanjel.RequestManagement.Core/Entities/Notification.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace Sanjel.RequestManagement.Core.Entities;
 
@@ -12,65 +9,65 @@ namespace Sanjel.RequestManagement.Core.Entities;
 public class Notification
 {
 
-    /// <summary>
-    /// notification_id property
-    /// </summary>
-    [Column("notification_id")]
-    [Key]
-    [Required]
-    [MaxLength(255)]
-    public string NotificationId { get; set; }
+	/// <summary>
+	/// notification_id property
+	/// </summary>
+	[Column("notification_id")]
+	[Key]
+	[Required]
+	[MaxLength(255)]
+	public string NotificationId { get; set; }
 
-    /// <summary>
-    /// request_id property
-    /// </summary>
-    [Column("request_id")]
-    [Key]
-    [Required]
-    [MaxLength(255)]
-    public string RequestId { get; set; }
+	/// <summary>
+	/// request_id property
+	/// </summary>
+	[Column("request_id")]
+	[Key]
+	[Required]
+	[MaxLength(255)]
+	public string RequestId { get; set; }
 
-    /// <summary>
-    /// recipient_type property
-    /// </summary>
-    [Column("recipient_type")]
-    public RecipientEnum RecipientType { get; set; }
+	/// <summary>
+	/// recipient_type property
+	/// </summary>
+	[Column("recipient_type")]
+	public RecipientEnum RecipientType { get; set; }
 
-    /// <summary>
-    /// notification_type property
-    /// </summary>
-    [Column("notification_type")]
-    public NotificationTypeEnum NotificationType { get; set; }
+	/// <summary>
+	/// notification_type property
+	/// </summary>
+	[Column("notification_type")]
+	public NotificationTypeEnum NotificationType { get; set; }
 
-    /// <summary>
-    /// delivery_method property
-    /// </summary>
-    [Column("delivery_method")]
-    public DeliveryEnum DeliveryMethod { get; set; }
+	/// <summary>
+	/// delivery_method property
+	/// </summary>
+	[Column("delivery_method")]
+	public DeliveryEnum DeliveryMethod { get; set; }
 
-    /// <summary>
-    /// sent_date property
-    /// </summary>
-    [Column("sent_date")]
-    public DateTime SentDate { get; set; }
+	/// <summary>
+	/// sent_date property
+	/// </summary>
+	[Column("sent_date")]
+	public DateTime SentDate { get; set; }
 
-    /// <summary>
-    /// content property
-    /// </summary>
-    [Column("content")]
-    [Required]
-    [MaxLength(255)]
-    public string Content { get; set; }
+	/// <summary>
+	/// content property
+	/// </summary>
+	[Column("content")]
+	[Required]
+	[MaxLength(255)]
+	public string Content { get; set; }
 
-    /// <summary>
-    /// action_buttons property
-    /// </summary>
-    [Column("action_buttons")]
-    [Required]
-    [MaxLength(255)]
-    public ICollection<string> ActionButtons { get; set; } = new List<string>();
+	/// <summary>
+	/// action_buttons property
+	/// </summary>
+	[Column("action_buttons")]
+	[Required]
+	[MaxLength(255)]
+	public ICollection<string> ActionButtons { get; set; } = new List<string>();
 
-    // Navigation Properties
+	// Navigation Properties
 
-    public virtual Request? Request { get; set; }
+	public virtual Request? Request { get; set; }
 }
